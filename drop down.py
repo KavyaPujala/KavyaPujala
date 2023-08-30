260b13f14792054200766345dad0a1dfac409e37
KavyaPujala <143532616+KavyaPujala@users.noreply.github.com>
2023-08-30 08:12:06 +0000

    Add files via upload

---
+++ drop down.py
@@ -0,0 +1,15 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+driver=webdriver.Chrome()
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+drp=Select(driver.find_element(By.NAME,"dropdown-class-example"))
+drp.select_by_value("option2")
+time.sleep(5)
+
+
+
+
