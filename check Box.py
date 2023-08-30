1cc22205c048287b6b52681ca0a7ceb1934aedde
KavyaPujala <143532616+KavyaPujala@users.noreply.github.com>
2023-08-30 08:08:17 +0000

    Add files via upload

---
+++ check Box.py
@@ -0,0 +1,11 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+driver=webdriver.Chrome()
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+driver.find_element(By.XPATH,"//input[@id='checkBoxOption1']").click()
+
+time.sleep(5)
